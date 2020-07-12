0330eba95926e2cd299784c944287c6029b3cf36
Olena Savchenko <olsavchenko1@gmail.com>
2020-07-12 23:09:20 +0300

    Updates

---
+++ precache-manifest.4356a2f2388a186a7a674a60dd695c7f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29aa4f0880af1b05133d9506f32cdac4",
+    "url": "/goit-react-hw-04-movies/index.html"
+  },
+  {
+    "revision": "d38a4c5cae3c3bbe42ef",
+    "url": "/goit-react-hw-04-movies/static/css/6.62d1d81a.chunk.css"
+  },
+  {
+    "revision": "f9d7ba4fdd44485c25d9",
+    "url": "/goit-react-hw-04-movies/static/css/home-view.53f14d61.chunk.css"
+  },
+  {
+    "revision": "3ce4f95f0f6b03ae516d",
+    "url": "/goit-react-hw-04-movies/static/css/main.c1dae506.chunk.css"
+  },
+  {
+    "revision": "422ad29940ab83072cd4",
+    "url": "/goit-react-hw-04-movies/static/css/movie-view.b27893c6.chunk.css"
+  },
+  {
+    "revision": "b9ad96b46853b012a632",
+    "url": "/goit-react-hw-04-movies/static/css/movies-view.6347501c.chunk.css"
+  },
+  {
+    "revision": "bf5e2ff890e8702bee2b",
+    "url": "/goit-react-hw-04-movies/static/js/0.bef6c41f.chunk.js"
+  },
+  {
+    "revision": "d38a4c5cae3c3bbe42ef",
+    "url": "/goit-react-hw-04-movies/static/js/6.4bbcc530.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-04-movies/static/js/6.4bbcc530.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9d7ba4fdd44485c25d9",
+    "url": "/goit-react-hw-04-movies/static/js/home-view.a9d79d5f.chunk.js"
+  },
+  {
+    "revision": "3ce4f95f0f6b03ae516d",
+    "url": "/goit-react-hw-04-movies/static/js/main.dd44715f.chunk.js"
+  },
+  {
+    "revision": "422ad29940ab83072cd4",
+    "url": "/goit-react-hw-04-movies/static/js/movie-view.21a03bdb.chunk.js"
+  },
+  {
+    "revision": "b9ad96b46853b012a632",
+    "url": "/goit-react-hw-04-movies/static/js/movies-view.f9650d61.chunk.js"
+  },
+  {
+    "revision": "46da4b6039875c74affe",
+    "url": "/goit-react-hw-04-movies/static/js/runtime-main.e0550e45.js"
+  },
+  {
+    "revision": "440b9b231a84584c31de471bd31d0985",
+    "url": "/goit-react-hw-04-movies/static/media/default.440b9b23.jpg"
+  }
+]);
